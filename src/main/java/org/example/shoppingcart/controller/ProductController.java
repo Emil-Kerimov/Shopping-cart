0eac1c1fb7deadaf74ff24040195112c795c6559
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-09-25 23:01:11 +0300

    finish getAll endpoint for ProductController

--- src/main/java/org/example/shoppingcart/controller/ProductController.java
+++ src/main/java/org/example/shoppingcart/controller/ProductController.java
@@ -1,4 +1,25 @@
 package org.example.shoppingcart.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.shoppingcart.models.Product;
+import org.example.shoppingcart.response.ApiResponse;
+import org.example.shoppingcart.service.product.IProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(new ApiResponse("success", products));
+    }
 }
