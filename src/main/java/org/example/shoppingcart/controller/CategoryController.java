5a0d4af555715b99e1c1758a40c7bd6b0e64638b
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-09-25 10:03:51 +0300

    add /all endpoint in CategoryController

--- src/main/java/org/example/shoppingcart/controller/CategoryController.java
+++ src/main/java/org/example/shoppingcart/controller/CategoryController.java
@@ -1,4 +1,34 @@
 package org.example.shoppingcart.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.shoppingcart.models.Category;
+import org.example.shoppingcart.response.ApiResponse;
+import org.example.shoppingcart.service.category.ICategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+
 }
