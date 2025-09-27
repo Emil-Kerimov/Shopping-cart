bda39dd064317ffe84c2968768d55b134c51f2f3
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-09-27 21:14:50 +0300

    finish ProductController

--- src/main/java/org/example/shoppingcart/controller/ProductController.java
+++ src/main/java/org/example/shoppingcart/controller/ProductController.java
@@ -131,4 +131,15 @@ public class ProductController {
             return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
         }
     }
+
+    @GetMapping("/product/count/by-brand/and-name")
+    public ResponseEntity<ApiResponse> countProductsBrandAndName(@RequestParam String brand, @RequestParam String name) {
+        try {
+            var productCount =  productService.countProductsByBrandAndName(brand, name);
+            return ResponseEntity.ok(new ApiResponse("Product count", productCount));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
 }
