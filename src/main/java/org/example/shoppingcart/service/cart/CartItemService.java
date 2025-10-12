1df8098d5e4a41017bdbdcef79e282f121be18b4
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-10-13 02:16:14 +0300

    add addItemToCart method implementation in CartItemService

--- src/main/java/org/example/shoppingcart/service/cart/CartItemService.java
+++ src/main/java/org/example/shoppingcart/service/cart/CartItemService.java
@@ -1,4 +1,52 @@
 package org.example.shoppingcart.service.cart;
 
-public class CartItemService {
+import lombok.RequiredArgsConstructor;
+import org.example.shoppingcart.models.Cart;
+import org.example.shoppingcart.models.CartItem;
+import org.example.shoppingcart.models.Product;
+import org.example.shoppingcart.repository.CartItemRepository;
+import org.example.shoppingcart.repository.CartRepository;
+import org.example.shoppingcart.service.product.IProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final IProductService productService;
+    private final ICartService cartService;
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+                .stream().
+                filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElse(new CartItem());
+        if(cartItem.getId() == null){
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        }
+        else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+
+    }
 }
--- src/main/java/org/example/shoppingcart/service/cart/ICartItemService.java
+++ src/main/java/org/example/shoppingcart/service/cart/ICartItemService.java
@@ -1,4 +1,7 @@
 package org.example.shoppingcart.service.cart;
 
 public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
 }
