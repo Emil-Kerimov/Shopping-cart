0c202e72beab3cfadac5b1d16cc61ee1a4315f16
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-11-17 02:13:54 +0200

    implements the order service and create user entity

---
+++ src/main/java/org/example/shoppingcart/models/User.java
@@ -0,0 +1,32 @@
+package org.example.shoppingcart.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String firstName;
+    private String lastName;
+    @NaturalId
+    private String email;
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+}
--- src/main/java/org/example/shoppingcart/service/order/IOrderService.java
+++ src/main/java/org/example/shoppingcart/service/order/IOrderService.java
@@ -2,7 +2,11 @@ package org.example.shoppingcart.service.order;
 
 import org.example.shoppingcart.models.Order;
 
+import java.util.List;
+
 public interface IOrderService {
     Order placeOrder(Long userId);
     Order getOrder(Long orderId);
+
+    List<Order> getUserOrders(Long userId);
 }
