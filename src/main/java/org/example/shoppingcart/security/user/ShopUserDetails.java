0152e2b9bacfb75701bdf04a78c5d8d8caed6d59
Emil Kerimov <emilkerimovofficial@gmail.com>
2025-12-12 00:57:53 +0200

    add Role model and changes in User Entity

---
+++ src/main/java/org/example/shoppingcart/security/user/ShopUserDetails.java
@@ -0,0 +1,44 @@
+package org.example.shoppingcart.security.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ShopUserDetails implements UserDetails {
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
